b56faa39c5c677235b93282e4d8fa6380e25f879
Tiago Cunha <tiagocunha.me@gmail.com>
2025-01-15 11:28:57 -0300

    feat(sh): setup ssh keys and install fonts

--- setup.sh
+++ setup.sh
@@ -8,5 +8,14 @@ cd $TEMP
 git clone https://github.com/cstiago/dotfiles.git
 cd dotfiles
 
+# Setup SSH keys
+ssh-keygen -t ed25519 -C $(hostnamectl | grep Static | awk '{print $3}')
+
 # Install packages from APT
 sed 's/#.*//' install/apt.txt | xargs sudo apt -y install
+
+# Install fonts
+cat install/fonts.txt | xargs wget -P fonts
+mkdir -p ~/.local/share/fonts
+cp fonts/*.ttf ~/.local/share/fonts
+fc-cache -f -v
