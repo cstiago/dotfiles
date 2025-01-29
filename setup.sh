61b6faf3faf69babcb0bc0a6a1bcffb305ac2ab4
Tiago Cunha <tiagocunha.me@gmail.com>
2025-01-29 16:24:57 -0300

    feat(setup): configure postfix for emacs package and add flathub

--- setup.sh
+++ setup.sh
@@ -11,14 +11,21 @@ cd dotfiles
 # Setup SSH keys
 ssh-keygen -t ed25519 -C $(hostnamectl | grep Static | awk '{print $3}')
 
+# Set package options
+sudo debconf-set-selections <<< "postfix postfix/mailname string $(hostname --long).localdomain"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
 # Install packages from APT
-sed 's/#.*//' install/apt.txt | xargs sudo apt -y install
+sed 's/#.*//' inst/apt.txt | xargs -n 1 sudo apt -y install
+
+# Add Flathub repository to Flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install packages from Flatpak
-sed 's/#.*//' install/flatpak.txt | xargs flatpak --or-update -y install 
+sed 's/#.*//' inst/flatpak.txt | xargs -n 1 flatpak --or-update -y install 
 
 # Install fonts
-cat install/fonts.txt | xargs wget -P fonts
+cat inst/fonts.txt | xargs wget -P fonts
 mkdir -p ~/.local/share/fonts
 cp fonts/*.ttf ~/.local/share/fonts
 fc-cache -f -v
